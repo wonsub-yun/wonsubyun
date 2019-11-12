4bfe80e49f35b62d129a0264f4cd062327a814d4
wonsub-yun <4kgrubby@naver.com>
2019-11-12 16:34:20 +0900

    final xml_change file

--- change_xml.py
+++ change_xml.py
@@ -1,35 +1,17 @@
 import xml.etree.ElementTree as ET
 import os
-'''
-#xml helpme###에 대해서:
-#경로 불러오기
-for
-    #name.set('updated', 'yes')
-#동일한 파일명으로 저장?
-       
-        print(root)
-        
-        
-'''
-
-
-
-a="/home/wonsub/Desktop/1111"
-b=os.listdir(a)
-path = os.getcwd()
-print(path)
-for c in b:
-    if c.endswith(".xml"):     
-        path_xml=a+"/"+c
+original_directory="/home/wonsub/Desktop/1111"
+list_of_files=os.listdir(original_directory)
+for xml_files in list_of_files:
+    if xml_files.endswith(".xml"):     
+        path_xml=original_directory+"/"+xml_files
         tree = ET.parse(path_xml)
         root = tree.getroot()
         for name in root.iter('name'):
-            print(name[0])
-            '''
             new_name = "b"
             name.text = str(new_name)
-            tree.write(c)
-            '''
+            os.chdir(original_directory+"/")
+            tree.write(xml_files)
         
        
         
